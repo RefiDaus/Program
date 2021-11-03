b8b39b2566ebc55333187fbba97cb11bc3d63b40
RefiDaus <refirefi12@gmail.com>
2021-11-04 02:16:09 +0700

    menambah latihan 1

---
+++ Tugas/latihan1.py
@@ -0,0 +1,11 @@
+#kondisi if adalah kondisi yang akan di eksekusi oleh program jika bernilai benar atau true
+
+nilai = 9
+
+#jika kondisi benar/true maka program akan mengeksekusi printah di bawah
+if(nilai > 5) :
+    print("sepuluh lebih besar dari angka lima") # kondisi benar, dieksekusi
+
+#jika kondisi salah/false maka program tidak akan mengeksekusi printah di bawah
+if(nilai > 10) :
+    print("sembilan lebih besar dari angka sepuluh") # kondisi salah mak tidak tereksekusi
