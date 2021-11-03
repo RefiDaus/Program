e5aa14281d0d6563fd3a5cb28080a03267fc2ade
RefiDaus <refirefi12@gmail.com>
2021-11-04 02:17:14 +0700

    menambah latihan 2

---
+++ Tugas/latihan2.py
@@ -0,0 +1,11 @@
+# cara menguraikan data di dalam list pyton
+
+#inisialisasi variable luas dengan nilai berformat
+luas = [34 , 56, 1, 3, 4, 200]
+
+#urutkan data di dalam list dari yang terkecil ke yang terbesar
+luas.sort()
+
+#tampilan nilai dari variable luas setelah pengurutan
+print(luas)
+[1, 3, 4, 34, 56, 200]
